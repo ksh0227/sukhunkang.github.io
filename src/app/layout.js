0f69280d15761bc443bc23aa4d2ec287c3b6d53e
bigboisw <wsean314@gmail.com>
2025-03-30 20:49:47 -0700

    testing env var

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import "./globals.css";
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Script from "next/script";
-import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +23,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}></Script>
+        <Script id="google-analytics">
+          {
+            `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+            `
+          }
+        </Script>
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={geistSans.className}>
@@ -34,9 +46,7 @@ export default function RootLayout({ children }) {
           </main>  
           <Footer />
         </div>
-        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_MEASUREMENT_ID} />
       </body>
     </html>
   );
 }
-
