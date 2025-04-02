24531a00785ec02b779d0236c7c49171e36d6669
bigboisw <wsean314@gmail.com>
2025-04-02 14:00:48 -0700

    google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,9 @@
-'use client';
-
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Script from "next/script";
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,38 +15,29 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-// export const metadata = {
-//   title: "Suhkun Kang | UC Santa Barbara",
-//   description: "Suhkun Kang | UC Santa Barbara",
-// };
+export const metadata = {
+  title: "Suhkun Kang | UC Santa Barbara",
+  description: "Suhkun Kang | UC Santa Barbara",
+};
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname(); // Get current route
-
-  useEffect(() => {
-    if (window.gtag) {
-      window.gtag("config", process.env.NEXT_PUBLIC_MEASUREMENT_ID, {
-        page_path: pathname,
-      });
-    }
-  }, [pathname]); // Runs when the route changes
 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        {/* Google Analytics script */}
-        <Script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
-        />
-        <Script id="google-analytics">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
-          `}
-        </Script>
+        {/* <!-- Google tag (gtag.js) --> */}
+      <Script async src={`https://www.googletagmanager.com/gtag/js?id=G-5XRGPSLYNT`}></Script>
+              <Script id="google-analytics">
+                {
+                  `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-5XRGPSLYNT');
+                  `
+                }
+              </Script>
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
